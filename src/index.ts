690088668052755c301f254a168433789e4d5101
Ishaan Puniani <ishaan.puniani@netent.com>
2019-02-25 19:05:37 +0100

    Added parameter to be read from command line

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
 import Creator from './helper';
-
 const creator = new Creator();
-
+const args = require('minimist')(process.argv.slice(2))
 if (creator.createReadme()) {
-  creator.insertBadges();
+  let summaryPath = args['summary'];
+  if (!summaryPath || summaryPath.length <= 0) {
+    summaryPath = undefined;
+  }
+  creator.insertBadges(summaryPath);
 }
